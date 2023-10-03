14ab439268ae97721e800a6dff91edb02634adf3
RizkyMaulita <rizky.maulita@gmail.com>
2023-10-03 16:23:07 +0700

    change error handler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -5,6 +5,18 @@ function errorHandler(err, req, res, next) {
   if (err.name === "Forbidden") {
     status = 403;
     message = `You aren't authorized`;
+  } else if (error.name === "SequelizeValidationError") {
+    status = 400;
+    message = error.errors[0].message;
+  } else if (error.name === `Username Empty`) {
+    status = 400;
+    message = `Username can't be empty`;
+  } else if (error.name === `Password Empty`) {
+    status = 400;
+    message = `Password can't be empty`;
+  } else if (error.name === "InvalidLogin") {
+    status = 401;
+    message = "Invalid email/password";
   }
 
   res.status(status).json({ message });
